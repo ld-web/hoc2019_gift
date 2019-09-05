a70b5b62e212d5d129bf53ce7a6c932f5c3b4c50
Lucas Delobelle <lucas@ld-web.net>
2019-09-05 09:53:21 +0200

    Added Gift Entity

---
+++ src/Repository/GiftRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Gift;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Gift|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Gift|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Gift[]    findAll()
+ * @method Gift[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class GiftRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Gift::class);
+    }
+
+    // /**
+    //  * @return Gift[] Returns an array of Gift objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('g.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Gift
+    {
+        return $this->createQueryBuilder('g')
+            ->andWhere('g.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
