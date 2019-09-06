f13880c9a0f40183fe66fd2b90502a31c1134869
Lucas Delobelle <lucas@ld-web.net>
2019-09-06 17:01:56 +0200

    User authentication + user fixtures (TODO : redirect after login success)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,35 @@
 namespace App\DataFixtures;
 
 use App\Entity\Gift;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+
+        $user->setEmail('bob@bob.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->encoder->encodePassword(
+                $user,
+                "blablabla"
+            ));
+
+        $manager->persist($user);
+        $manager->flush();
+
         $faker = Factory::create('fr_FR');
 
         for ($i = 0; $i < 10; $i++) {
